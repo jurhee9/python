119575c8b155319b46352285e73660129860d762
Juha Lee <83819477+naperhee@users.noreply.github.com>
2021-11-03 23:58:22 +0900

    Create asgmt.py

---
+++ asgmt.py
@@ -0,0 +1,14 @@
+def computepay(h,r):
+    if h > 40:
+        pa = 40 * r + (h-40)*1.5*r
+    else:
+        pa = h * r
+    return pa
+
+hrs = input("Enter Hours:")
+h = float(hrs)
+rate = input("Enter Rate")
+r = float(rate)
+p = computepay(h,r)
+
+print("Pay",p)
